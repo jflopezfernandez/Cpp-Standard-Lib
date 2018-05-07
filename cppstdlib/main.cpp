383ac17bf853226307cc7d36a4c9cb17b48921b5
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-05-07 06:54:16 -0400

    Successfully used std::regex to match phone numbers in the most common every- day formats.

--- cppstdlib/main.cpp
+++ cppstdlib/main.cpp
@@ -1,5 +1,11 @@
 
+#include <iomanip>
 #include <iostream>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <vector>
 
 #include <Catch/catch.hpp>
 
@@ -7,8 +13,41 @@
 
 #include <gsl/gsl>
 
-
-int main()
+inline std::string Found(const bool result)
 {
-    std::cout << "testing...\n";
+    return ((result) ? "Found" : "Not found");
+}
+
+
+int main(int argc, char *argv[])
+{
+    const std::string defaultRegex { "\\(?\\d{3}[-\\) ]+\\d{3}[- ]?\\d{4}" };
+
+    std::regex phoneNumberRegex;
+
+    if (argc == 1)
+    {
+        std::cout << "[No regex supplied, using default]: /" << defaultRegex << "/\n";
+
+        phoneNumberRegex = defaultRegex;
+    } else
+    {
+        phoneNumberRegex = std::regex { argv[1] };
+    }
+
+    std::vector<std::string> phoneNumbers = {
+        "813-555-0123",
+        "012 344 0123",
+        "(813) 234-0211",
+        "(124)888-1002"
+    };
+
+    for (auto& phoneNumber : phoneNumbers)
+    {
+        std::cout << "[" << phoneNumber << "]: " << std::boolalpha << std::regex_match(phoneNumber, phoneNumberRegex) << "\n";
+    }
+
+    
+
+    //std::regex_match("XML tag: <tag-name>the value</tag-name>.", m, std::regex("<(.*)>(.*)</(\\1)>"));
 }
